dea675c6cc79580959fda7f36a4f56cd974b2f82
Vaibhav Saxena <NIPC21@192.168.0.70>
2017-07-15 01:31:30 +0530

    Configured db

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database : 'mongodb://localhost:27017/users',
+    database : 'mongodb://admin_foodie:admin_foodie123@ds063725.mlab.com:63725/foodie',
     secret : 'YourSecret',
     nexmoScret:'aabc3598311d9e70',
     nexmoAPIKey : 'babde38e'
